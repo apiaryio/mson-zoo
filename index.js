7f9713ceab1ebe1626c364d5fa1504baa264d94d
XVincentX <vincenz.chianese@yahoo.it>
2015-09-07 18:07:48 +0200

    exporting a json object

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var fs = require('fs')
 
 module.exports.samples = fs.readdirSync(__dirname + '/samples').map(function(file){
-	return fs.readFileSync(__dirname + '/samples/' + file, {encoding: 'utf8'});
+	return {
+		name: file,
+		code: fs.readFileSync(__dirname + '/samples/' + file, {encoding: 'utf8'}),	
+	};
 });
