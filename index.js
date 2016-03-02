caec8356446d5951e11624ce923b91113ed4d7f3
František Hába <frantisek@apiary.io>
2016-03-02 16:28:30 +0100

    Add missing space.

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var SAMPLES_FOLDER_PATH = __dirname + '/samples';
 module.exports.samples = fs.readdirSync(SAMPLES_FOLDER_PATH).map(function(filePath) {
   return {
     name: filePath,
-    code: fs.readFileSync(SAMPLES_FOLDER_PATH+ '/' + filePath, {encoding: 'utf8'}),
+    code: fs.readFileSync(SAMPLES_FOLDER_PATH + '/' + filePath, {encoding: 'utf8'}),
   };
 });
