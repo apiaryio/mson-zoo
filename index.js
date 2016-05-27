bba2dc802101e47f7681af1831d7cc2247d1ec0f
František Hába <frantisek@apiary.io>
2016-05-27 02:33:40 +0200

    Do not exclude the header.

--- index.js
+++ index.js
@@ -6,18 +6,13 @@ var PARSED_PATH = path.join(__dirname, 'samples-parsed');
 
 module.exports.samples = fs.readdirSync(SAMPLES_PATH).map(function (filePath) {
   var mson = fs.readFileSync(path.join(SAMPLES_PATH, filePath), 'utf8');
+
   var structures = JSON.parse(
     fs.readFileSync(
       path.join(PARSED_PATH, filePath.replace('.md', '.json')), 'utf8'
     )
   );
 
-  // We *exclude* the header so the MSON is easier to use in places like
-  // e.g. API Blueprint `+ Attributes` sections, which you can build up
-  // via the `sample.type` and `sample.mson`.
-  if (mson[0] === '#') {
-    mson = mson.split('\n').slice(1).join('\n');
-  }
   return ({
     name: filePath,
     mson: mson,
